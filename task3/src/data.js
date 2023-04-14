c9839022553e1b82d41092b85a73cd3b5739ff90
ck-19 <127390990+ck-19@users.noreply.github.com>
2023-04-14 20:23:50 +0530

    Add files via upload

---
+++ task3/src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+      "Brand:	Google ,Model Name:Pixel 6 128gb, Network Service Provider:	Unlocked for All Carriers, OS: Android 12.0, Cellular Technology:	LTE, Memory Storage Capacity:	128 GB, Colour:	Black, Screen Size:6.4 Inches, Wireless network technology:	GSM, Wi-Fi,  Connector Type: USB type C",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "The Samsung Galaxy S7 features a 5.1-inch Quad HD Super AMOLED screen, Qualcomm's most powerful Snapdragon 820 system chip under the hood and a brand new 12-megapixel camera with low light superpowers. All of that runs on a new edition of TouchWiz on top of Android 6 Marshmallow. And yes, the microSD card slot is back: you can use a card of up to 200GB onf the S7. The Galaxy S7 also has a very large, 3000mAh battery and it adds IP68 water resistance.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 8,
+    company: "htc",
+    info:
+      "HTC 10, 4GB RAM with 32GB inbuilt memory costs Rs. 35,000. The phone measures 5.74 x 2.83 x 0.35 and weighs 161 gm. Its internal memory can be upgraded to 2TB via a microSD card. It has a 5 inch Super LCD3 capacitive touchscreen with 16M colors and Corning Gorilla Glass 4, which comes with a resolution of 2560 x 1440 pixels. It has a 5MP front camera with optical image stabilization and a 12.3MP Sony Exmor R IMX377 rear camera with a 26mm lens which also has optical image stabilization . It has a 3,000 mAh Li-ion battery, and runs Android 8.0 (Lollipop) using a Qualcomm Snapdragon 820. For graphics, it has a Adreno 530 GPU. One of the highly praised features of the phone is its audio quality, made possible by the dedicated DAC (Digital Audio Converter) provided by Qualcomm.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "HTC 10, 4GB RAM with 32GB inbuilt memory costs Rs. 35,000. The phone measures 5.74 x 2.83 x 0.35 and weighs 161 gm. Its internal memory can be upgraded to 2TB via a microSD card. It has a 5 inch Super LCD3 capacitive touchscreen with 16M colors and Corning Gorilla Glass 4, which comes with a resolution of 2560 x 1440 pixels. It has a 5MP front camera with optical image stabilization and a 12.3MP Sony Exmor R IMX377 rear camera with a 26mm lens which also has optical image stabilization . It has a 3,000 mAh Li-ion battery, and runs Android 8.0 (Lollipop) using a Qualcomm Snapdragon 820. For graphics, it has a Adreno 530 GPU.One of the highly praised features of the phone is its audio quality, made possible by the dedicated DAC (Digital Audio Converter) provided by Qualcomm..",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "TC Desire 626s Android Smartphone was launched in July 2015. It is priced in India at Rs. 5380 ($78.99), running on the Qualcomm MSM8909 Snapdragon 210 processor with Adreno 304 GPU. The phone has 1 GB RAM and 8 GB of internal storage which is expandable using microSD, up to 32 GB.The HTC Desire 626s has a 5.0 inches HD, 720 x 1280 pixels TFT LCD display with a pixel density of 294 ppi. Coming to cameras, it has a 8 MP rear camera and a 2 MP front camera for selfies. It has a microUSB port for data syncing and charging. It is a single SIM smartphone with support for 4G LTE, Wi-Fi, Bluetooth and GPS too.The HTC Desire 626s runs on Android OS, v5.1 (Lollipop) and all this is powered by a 2000 mAh battery. It measures 146.8 x 70.9 x 8.1 mm (height x width x thickness) with a total weight of 138.9 grams including battery.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 30,
+    company: "apple",
+    info:
+      "The Apple iPhone 7 is the successor to the highly acclaimed iPhone 6s. As such, it brings a multitude of enhancements in key areas, including design, performance, and user experience. While the overall shape and size of the phone have been left intact, there are now glossy and matte black options available. Also, the handset is now IP67-certified, making it water-resistant. On its back, the refined 12MP camera features optical image stabilization, wider aperture of F1.8, and a quad LED TrueTone flash for better low-light performance. Battery life has been given a welcome boost that could provide users around 2 hours of additional use time. The new A10 Fusion chip is up to 40% more powerful than last-year's A9, yet promises great power efficiency. And while the iPhone 7 lacks an audio jack, the box includes Lightning connector EarPods, as well as a Lightning to 3.5mm adapter.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 2,
+    company: "apple",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Brand:	Google ,Model Name:Pixel 6 128gb, Network Service Provider:	Unlocked for All Carriers, OS: Android 12.0, Cellular Technology:	LTE, Memory Storage Capacity:	128 GB, Colour:	Black, Screen Size:6.4 Inches, Wireless network technology:	GSM, Wi-Fi,  Connector Type: USB type C",
+  inCart: false,
+  count: 0,
+  total: 0
+};
